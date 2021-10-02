74f405a8c2f43ba6e74d677da974fb98555a5252
Adam <adam.butakov@yandex.ru>
2021-10-02 21:34:25 +0300

    Create 4.1.cs

---
+++ Lab4/4.1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab4But
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Даны стороны прямоугольника a и b. Найти его площадь S = a·b и периметр P = 2·(a + b).");
+            Console.WriteLine("Введите значение а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Площадь : {a * b} ");
+            Console.WriteLine($"Периметр :{2 * (a + b)}");
+
+        }
+    }
+}
