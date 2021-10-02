45c49f1574992944d19724d770aef3d775f34b45
Adam <adam.butakov@yandex.ru>
2021-10-02 21:34:32 +0300

    Create 4.4.cs

---
+++ Lab4/4.4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("4. Даны два ненулевых числа. Найти сумму, разность, произведение и частное их квадратов.");
+            Console.WriteLine("Введите первое число ");
+            float first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число ");
+            float sec = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сумма чисел:{(first * first) + (sec * sec)}");
+            Console.WriteLine($"Разность чисел:{(first * first) - (sec * sec)}");
+            Console.WriteLine($"Произведение чисел:{(first * first) * (sec * sec)}");
+            Console.WriteLine($"Частное квадратов чисел:{(first * first) / (sec * sec)}");
+
+
+        }
+    }
+}
