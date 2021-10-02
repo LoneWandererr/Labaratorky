5dba8d6f65584380b08f611c15548f2598eacd80
Adam <adam.butakov@yandex.ru>
2021-10-02 21:34:30 +0300

    Create 4.2.cs

---
+++ Lab4/4.2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. Дан диаметр окружности d. Найти ее длину L = π·d. В качестве значения π использовать 3.14.");
+            Console.WriteLine("Введите значение d");
+            int d = Convert.ToInt32(Console.ReadLine());
+            double L = 3.14 * d;
+            Console.WriteLine($"Диаметр окружности равен: {L}");
+
+        }
+    }
+}
