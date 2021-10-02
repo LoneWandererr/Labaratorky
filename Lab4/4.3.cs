d472fb19f9d69ab277bc6d3ed090c3c3c9bd1d4c
Adam <adam.butakov@yandex.ru>
2021-10-02 21:31:22 +0300

    Create 4.3.cs

---
+++ Lab4/4.3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3. Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2.");
+            Console.WriteLine("Введите а");
+            float a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            float b = Convert.ToInt32(Console.ReadLine());
+            double Middle = (a + b) / 2;
+            Console.WriteLine($"Среднее арифметическое:{Middle}");
+        }
+    }
+}
