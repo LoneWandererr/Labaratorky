5fa6221b39f82ca49e1f97660cbeee09a311d8b8
Adam <adam.butakov@yandex.ru>
2021-10-02 21:34:35 +0300

    Create 4.5.cs

---
+++ Lab4/4.5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5. Даны два ненулевых числа. Найти сумму, разность, произведение и частное их модулей.");
+            Console.WriteLine("Введите первое число ");
+            float first = Math.Abs((Convert.ToInt32(Console.ReadLine())));
+            Console.WriteLine("Введите второе число ");
+            float sec = Math.Abs((Convert.ToInt32(Console.ReadLine())));
+            Console.WriteLine($"Сумма чисел:{first + sec}");
+            Console.WriteLine($"Разность чисел:{first - sec}");
+            Console.WriteLine($"Произведение чисел:{first * sec}");
+            Console.WriteLine($"Частное чисел:{first / sec}");
+        }
+    }
+}
