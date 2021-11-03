f95c83e88bb8ce6ac3f9aca07e2322e8c45a82ff
Adam <adam.butakov@yandex.ru>
2021-11-03 16:47:10 +0300

    Create 6.4.cs

---
+++ Lab6/6.4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("4. Найти значение функции y = 3x^6 - 6x^2 - 7 при данном значении x");
+            int x = Convert.ToInt32(Console.ReadLine());        
+            double y = 3 * Math.Pow(x, 6) - 6 * Math.Pow(x, 2) - 7;
+            Console.WriteLine($"y = 3*{x}^6 - 6*{x}^2 - 7 = {y}");
+        }
+    }
+}
