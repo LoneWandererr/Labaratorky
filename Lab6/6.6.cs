88d4915478926c081f0c05d421c468062f4965d5
Adam <adam.butakov@yandex.ru>
2021-11-03 16:47:14 +0300

    Create 6.6.cs

---
+++ Lab6/6.6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _6._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("6. Дано число A. Вычислить A^8 , используя вспомогательную переменную и три операции умножения.");
+            int A = Convert.ToInt32(Console.ReadLine());
+            double b = Math.Pow(A,8) / Math.Pow(A,3);
+            double a8 = A * A * A * b;
+            Console.WriteLine($"A в 8 степени равна {a8}");
+        }
+    }
+}
