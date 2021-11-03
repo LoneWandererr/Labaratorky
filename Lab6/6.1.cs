dde2fff9f3eec60491f67809ca312a6b2d0c6937
Adam <adam.butakov@yandex.ru>
2021-11-03 16:47:03 +0300

    Create 6.1.cs

---
+++ Lab6/6.1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Поменять местами содержимое переменных A и B и вывести новые значения A и B.");
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"А = {A}, B = {B}");
+            int a1 = A;
+            int b1 = B;
+            A = b1;
+            B = a1;
+            Console.WriteLine($"А = {A}, B = {B}");
+
+        }
+        }
+}
