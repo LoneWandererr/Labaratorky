425b634e5c19a060cca38b4a9c5fbb9868e764b2
Adam <adam.butakov@yandex.ru>
2021-11-03 16:47:17 +0300

    Create 6.7.cs

---
+++ Lab6/6.7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("7. Дано число A. Вычислить A*15, используя две вспомогательные переменные и пять операций умножения.");
+            int A = Convert.ToInt32(Console.ReadLine());
+            double b = Math.Pow(A, 15) / Math.Pow(A, 10);
+            double c = Math.Pow(A, 15) / Math.Pow(A, 9);
+            double a8 = A * A * A * A * b * c;
+            Console.WriteLine($"A в 15 степени равна {a8}");
+        }
+    }
+}
