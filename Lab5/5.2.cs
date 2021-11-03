98585d2fe914a10e7c0a6936e0f9de399d47c3a1
Adam <adam.butakov@yandex.ru>
2021-11-03 15:51:18 +0300

    Update 5.2.cs

--- Lab5/5.2.cs
+++ Lab5/5.2.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Lab5._1
+namespace _5._2
 {
     class Program
     {
@@ -8,16 +8,16 @@ namespace Lab5._1
         {
             Console.WriteLine("1. Найти расстояние между двумя точками с заданными координатами (x1, y1) и (x2, y2)");
             Console.WriteLine("Введите координаты точки А");
-            int x1 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            int y1 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите координаты точки B");
-            int x2 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            int y2 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите координаты точки C");
-            int x3 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            int y3 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            double AC = Math.Pow((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1), 0.5);
-            double BC = Math.Pow((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2), 0.5);
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            double AC = Math.Pow(Math.Abs(x3 - x1) * Math.Abs(x3 - x1) + Math.Abs(y3 - y1) * Math.Abs(y3 - y1), 0.5);
+            double BC = Math.Pow(Math.Abs(x3 - x2) * Math.Abs(x3 - x2) + Math.Abs(y3 - y2) * Math.Abs(y3 - y2), 0.5);
             double sum = AC + BC;
             Console.WriteLine($"Длина АС {AC}");
             Console.WriteLine($"Длина BС {BC}");
