3f39b907754c5accf997a41ff501308026f1651c
Adam <adam.butakov@yandex.ru>
2021-11-03 16:47:12 +0300

    Create 6.5.cs

---
+++ Lab6/6.5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _6._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5. Найти значение функции y = 4(x−3)6 − 7(x−3)3 + 2 при данном значении x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            double y = 4 * (x - 3) * 6 - 7 * (x - 3) * 3 + 2;
+            Console.WriteLine($"4 * ({x} - 3) * 6 - 7 * ({x} - 3) * 3 + 2 = {y}");
+        }
+    }
+}
