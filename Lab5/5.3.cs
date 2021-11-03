6ed5a2324a4a7eedb494f175fd74dd11ab307538
Adam <adam.butakov@yandex.ru>
2021-11-03 15:51:21 +0300

    Update 5.3.cs

--- Lab5/5.3.cs
+++ Lab5/5.3.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 
-namespace Lab5._1
+namespace _5._3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Найти расстояние между двумя точками с заданными координатами (x1, y1) и (x2, y2)");
+            Console.WriteLine("3. Даны три точки A, B, C на числовой оси. Точка C расположенамежду точками A и B.Найти произведение длин отрезков AC и BC)");
             Console.WriteLine("Введите координаты точки А");
-            int x1 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            int y1 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите координаты точки B");
-            int x2 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            int y2 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите координаты точки C");
-            int x3 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            int y3 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            double AC = Math.Pow((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1), 0.5);
-            double BC = Math.Pow((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2), 0.5);
-            double sum = AC * BC;
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            double AC = Math.Pow(Math.Abs(x3 - x1) * Math.Abs(x3 - x1) + Math.Abs(y3 - y1) * Math.Abs(y3 - y1), 0.5);
+            double BC = Math.Pow(Math.Abs(x3 - x2) * Math.Abs(x3 - x2) + Math.Abs(y3 - y2) * Math.Abs(y3 - y2), 0.5);
+            double um = AC * BC;
             Console.WriteLine($"Длина АС {AC}");
             Console.WriteLine($"Длина BС {BC}");
-            Console.WriteLine($"Произведение отрезков {sum}");
+            Console.WriteLine($"Произведение длин отрезков {um}");
         }
     }
 }
