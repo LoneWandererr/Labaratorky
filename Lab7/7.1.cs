0138c382856332c57653e2c58ef4833fdb08ddb4
Adam <adam.butakov@yandex.ru>
2021-11-06 20:19:01 +0300

    Create 7.1.cs

---
+++ Lab7/7.1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Дано значение угла α в градусах (0 < α < 360). Определить значение этого же угла в радианах, учитывая, что 180◦ = π радианов. ");
+            Console.WriteLine("Введите значение а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{a} градусов это {a/(180/3.14)} радианов");
+                
+
+
+        }
+
+    }
+}
