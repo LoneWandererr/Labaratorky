aa853b4e7b59a881cf6e9b74413d096aa53b3994
Adam <adam.butakov@yandex.ru>
2021-11-06 20:19:08 +0300

    Create 7.3.cs

---
+++ Lab7/7.3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _7._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3. Известно, что X кг конфет стоит A рублей. Определить, сколько стоит 1 кг и Y кг этих же конфет.");
+            Console.WriteLine("Введите значение X кг");
+            int X = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение A рублей");
+            int A = Convert.ToInt32(Console.ReadLine());
+            double kg = A / X;
+            Console.WriteLine($"1 киллограмм таких конфет стоит {kg} р.");
+            Console.WriteLine("Введите вес, стоимость которого вы желаете узнать");
+            int Y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Стоимость {Y} кг равна {Y*kg} р.");
+
+
+
+
+        }
+    }
+}
