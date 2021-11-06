e25c4d0a84aa2deb101df0f04275c220dbdc9883
Adam <adam.butakov@yandex.ru>
2021-11-06 20:19:13 +0300

    Create 7.5.cs

---
+++ Lab7/7.5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5. Решить линейное уравнение A•x + B = 0, заданное своими коэффициентами A и B (коэффициент A не равен 0).");
+            Console.WriteLine("Введите значение А");
+            float A = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение B");
+            float B = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Уравнение : {A}x + {B} = 0");
+            double x = (-1 * B) / A;
+            Console.WriteLine($"Ответ: x = {x}");
+
+        }
+    }
+}
