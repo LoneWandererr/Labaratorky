0db03cb5852aa935cdc5581d08f7933fbb1713ec
Adam <adam.butakov@yandex.ru>
2021-11-06 20:19:05 +0300

    Create 7.2.cs

---
+++ Lab7/7.2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. Дано значение угла α в радианах (0 < α < 2•π). Определить значение этого же угла в градусах, учитывая, что 180◦ = π радианов");
+            Console.WriteLine("Введите значение а");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine($"{a} раиданов это {a * (180 / 3.14)} градусов");
+        }
+    }
+}
